940ebf234f72060e2dac3d8fc1b4affa0ee3289b
JV <joaovitoraraujo1821@gmail.com>
2025-02-28 10:24:21 -0300

    Correção endpoint listagem

--- ProductModel.py
+++ ProductModel.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Column, String, Float, Integer
+from sqlalchemy.ext.mutable import MutableDict
+from sqlalchemy.types import String
 from database import Base
-from sqlalchemy.ext.mutable import MutableList
-from sqlalchemy.types import JSON
+import json
 
 class Product(Base):
     __tablename__ = "products"
@@ -9,5 +10,12 @@ class Product(Base):
     id = Column(Integer, primary_key=True, index=True)
     ean = Column(String, unique=True, index=True)
     title = Column(String)
-    lowest_recorded_price = Column(Float, nullable=True)  # Pode ser None
-    images =  Column(MutableList.as_mutable(JSON))
+    lowest_recorded_price = Column(Float, nullable=True)
+    images = Column(String)  # Usando String para armazenar a lista JSON como texto
+
+    def set_images(self, images_list):
+        self.images = json.dumps(images_list)  # Armazena a lista como uma string JSON
+
+    def get_images(self):
+        return json.loads(self.images) if self.images else []  # Converte a string JSON de volta para uma lista
+
--- main.py
+++ main.py
@@ -20,27 +20,22 @@ def get_db():
 
 @app.post("/products/create", response_model=schemas.ProductResponse)
 def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
-    # Verifica se já existe um produto com o mesmo 'ean'
-    db_product = db.query(ProductModel.Product).filter(ProductModel.Product.ean == product.ean).first()
-    if db_product:
-        raise HTTPException(status_code=400, detail="Produto já existe com esse EAN")
 
-    # Armazena a lista de imagens como uma string JSON
-    images_json = json.dumps(product.images)  # Converte a lista de imagens para string JSON
-
-    # Cria o novo produto
     db_product = ProductModel.Product(
         ean=product.ean,
         title=product.title,
         lowest_recorded_price=product.lowest_recorded_price,
-        images=images_json  # Armazena como JSON
     )
+
+    # Usando set_images para converter a lista de imagens em string JSON
+    db_product.set_images(product.images)
 
     db.add(db_product)
     db.commit()
     db.refresh(db_product)
 
     return db_product
+
 
 @app.get('/products/findAll', response_model=list)
 def list_all(db: Session = Depends(get_db)):
